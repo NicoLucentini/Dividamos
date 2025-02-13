eecfdac1960101bc24d50a9808bea7450b7cda58
NicoLucentini <lucentini.nicolas@gmail.com>
2025-02-13 20:36:44 -0300

    Add all splitwise clone made up

---
+++ src/main/java/entities/Transaccion.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Transaccion{
+    public String acreedor;
+    public String deudor;
+    public float monto;
+
+    public Transaccion(String deudor, String acreedor, float monto){
+        this.acreedor = acreedor;
+        this.deudor = deudor;
+        this.monto = monto;
+    }
+    @Override
+    public String toString() {
+        return deudor + " paga $" + monto + " a " + acreedor;
+    }
+}
